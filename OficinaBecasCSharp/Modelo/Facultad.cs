b29412cae289172d18a6a49741c0dcb65e59d0ae
Ronie Paolo <ronie.arauco@pucp.edu.pe>
2018-04-10 23:31:11 -0500

    Anadiendo Facultad

---
+++ OficinaBecasCSharp/Modelo/Facultad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo {
+    class Facultad {
+        int idFacultad; //Puede ser un string
+        string nombre;
+        char pabellon;
+
+        public int IdFacultad { get => idFacultad; set => idFacultad = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public char Pabellon { get => pabellon; set => pabellon = value; }
+    }
+}
